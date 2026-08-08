6cc55ee77ea76cb1875140a08670d3ea98d32651
eissaalbothigi-cell <eissaalbothigi@gmail.com>
2026-08-08 20:45:51 +0300

    Update separator.tsx

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,31 +1,25 @@
-'use client';
-
-import * as SeparatorPrimitive from '@radix-ui/react-separator';
 import { cn } from '@/utils/Helpers';
 
-function Separator({
-  className,
-  orientation = 'horizontal',
-  decorative = true,
-  ...props
-}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+interface SeparatorProps {
+  orientation?: 'horizontal' | 'vertical';
+  className?: string;
+}
+
+/**
+ * مكون فاصل بسيط ونظيف (بدون حزم خارجية)
+ * يعمل بشكل مثالي في العربية والإنجليزية
+ */
+export function Separator({ orientation = 'horizontal', className }: SeparatorProps) {
   return (
-    <SeparatorPrimitive.Root
-      data-slot="separator"
-      decorative={decorative}
-      orientation={orientation}
+    <div
+      role="separator"
+      aria-orientation={orientation}
       className={cn(
-        `
-          shrink-0 bg-border
-          data-[orientation=horizontal]:h-px
-          data-[orientation=horizontal]:w-full
-          data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px
-        `,
-        className,
+        'shrink-0 bg-border',
+        orientation === 'horizontal' && 'h-px w-full',
+        orientation === 'vertical' && 'h-full w-px',
+        className
       )}
-      {...props}
     />
   );
 }
-
-export { Separator };
