ceff54d22a3608bcf44c09e12ccd6e63f08795bb
eissaalbothigi-cell <eissaalbothigi@gmail.com>
2026-08-17 15:24:53 +0300

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./src/models/Schema.ts",
-  out: "./drizzle",
+  schema: "./src/db/schema.ts",
+  out: "./drizzle/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
-  verbose: true,
-  strict: true,
 });
